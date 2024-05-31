17c6ffc0ef497af42da4c3c9feb9f70a00af2f08
Paul Ventura <j.paul.ventura@gmail.com>
2024-05-30 20:09:10 -0500

    Outlet, Link

--- src/components/R002MyNavbar.jsx
+++ src/components/R002MyNavbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Outlet, Link } from "react-router-dom";
 
 function R002MyNavbar() {
   return (
@@ -11,7 +12,7 @@ function R002MyNavbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">Home</a>
+              <a className="nav-link active" aria-current="page" href="/home">Home</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="/">Link</a>
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Outlet, Link } from "react-router-dom";
+
 import R002MyNavbar from '../components/R002MyNavbar'
 import R100Footer from '../components/R100Footer'
 
@@ -6,6 +8,7 @@ function Layout() {
   return (
     <div>
       <R002MyNavbar></R002MyNavbar>
+      <Outlet />
       <R100Footer></R100Footer>
     </div>
   )
